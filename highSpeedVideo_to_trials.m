b60423c738a250873163b002aad5b86c2a9b0731
kimerein <kimerein@users.noreply.github.com>
2022-01-24 16:16:24 -0500

    finding more rig events from high speed vid

--- highSpeedVideo_to_trials.m
+++ highSpeedVideo_to_trials.m
@@ -8,10 +8,16 @@ function highSpeedVideo_to_trials(filename,cueVarName,distractorVarName,rawVarNa
 % settings.distractorDuration = in seconds
 % settings.cueDuration = in seconds
 % settings.wheelSecBeforeCue = when wheel starts in seconds before cue
+% e.g.,
+%                    fps: 256
+%     distractorDuration: 0.2500
+%            cueDuration: 0.2500
+%      wheelSecBeforeCue: 0.9600
 
 % have user set these
 cueThresh=100;
-
+rawThresh=1; % for where video skips, this is after the wheel has been turning for a little bit, about 0.96 s in Kim's rig
+distractorThresh=100;
 
 a=load(filename);
 cueDiffs=a.(cueVarName);
@@ -31,8 +37,8 @@ if framesmax>30000
     framesmax=30000;
 end
 plot(cueDiffs(1:framesmax),'Color','b'); hold on;
-line([1 framesmax],[cueThresh cueThresh],'Color','r');
-line([1 framesmax],[-cueThresh -cueThresh],'Color','r');
+line([1 framesmax],[cueThresh cueThresh],'Color','b');
+line([1 framesmax],[-cueThresh -cueThresh],'Color','b');
 ylabel('Difference frame-to-frame in cue zone');
 xlabel('Frames');
 % find cues of specific duration
@@ -42,6 +48,34 @@ for i=1:length(cueStarts)
     line([cueStarts(i) cueEnds(i)],[cueThresh-cueThresh*0.25 cueThresh-cueThresh*0.25],'Color','c','LineWidth',2);
 end
 
+% get wheel turns beginning at a specific time before cue
+wheelOnsets=find(rawDiffs>rawThresh);
+comboDiffs=zeros(size(cueDiffs));
+comboDiffs(cueStarts)=-1;
+% only keep wheelOnsets closest to cue onset, because cue is clearer
+% signal
+wheelStarts=zeros(size(cueStarts));
+for i=1:length(cueStarts)
+    % find closest wheel onset preceding cue
+    wheelStarts(i)=wheelOnsets(find(wheelOnsets<cueStarts(i),1,'last'));
+end
+comboDiffs(wheelStarts)=1;
+[wheelStarts,wheelEnds]=findEventsOfSpecificDuration(comboDiffs,0.5,wheelBeforeCueInds,indsSlop);
+plot(rawDiffs(1:framesmax),'Color','g'); hold on;
+line([1 framesmax],[rawThresh rawThresh],'Color','g');
+for i=1:length(wheelStarts)
+    line([wheelStarts(i) wheelEnds(i)],[cueThresh-cueThresh*0.3 cueThresh-cueThresh*0.3],'Color',[0 0.75 0],'LineWidth',2);
+end
+
+% get distractors
+plot(distractorDiffs(1:framesmax),'Color','r'); hold on;
+line([1 framesmax],[distractorThresh distractorThresh],'Color','r');
+line([1 framesmax],[-distractorThresh -distractorThresh],'Color','r');
+[distractorStarts,distractorEnds]=findEventsOfSpecificDuration(distractorDiffs,distractorThresh,distractorInds,indsSlop);
+% plot
+for i=1:length(distractorStarts)
+    line([distractorStarts(i) distractorEnds(i)],[cueThresh-cueThresh*0.15 cueThresh-cueThresh*0.15],'Color','m','LineWidth',2);
+end
 
 end
 
@@ -49,6 +83,11 @@ function [cueStarts,cueEnds]=findEventsOfSpecificDuration(cueDiffs,cueThresh,cue
 
 fcue=find(cueDiffs>cueThresh);
 fcueoff=find(cueDiffs<-cueThresh);
+if length(fcueoff)>length(fcue)
+    fcueoff=fcueoff(1:length(fcue)); % truncate and drop the last events
+elseif length(fcue)>length(fcueoff)
+    fcue=fcue(1:length(fcueoff));
+end
 diff1=fcueoff-fcue; % if starts with cue on, will be positive
 diff2=fcueoff(2:end)-fcue(1:end-1); % if starts with cue off
 if mode(sign(diff1))<0
