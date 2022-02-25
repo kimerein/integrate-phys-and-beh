d9a32c942ac6b83ea8b042ea41f57ee121aac41c
kimerein <kimerein@users.noreply.github.com>
2022-02-24 19:27:06 -0500

    fixed bug in calculating movie sample rate in some cases

--- physiology_triggered_on_reach.m
+++ physiology_triggered_on_reach.m
@@ -73,9 +73,9 @@ disp(['found ' num2str(length(cueInds)) ' cues in totalalignment']);
 % Need to resample movie and arduino data so that indices represent
 % matching times
 settings.photo_fs=data.Fs; % photometry data sampling rate in Hz
-temp=totalalignment.timesfromarduino(2:end)./1000-totalalignment.timesfromarduino(1:end-1)./1000;
-temp=temp(temp~=0);
-settings.movie_fs=1/mode(temp); % movie data sampling rate in Hz
+settings.movie_fs=1/(mode(diff(alignment.timesfromarduino))/1000); % movie data sampling rate in Hz
+disp(['Check this. Movie data sampling rate is ' num2str(settings.movie_fs)]);
+pause;
 settings.scale_factor=floor(settings.photo_fs/settings.movie_fs);
 settings.photo_dec=settings.scale_factor-15;
 settings.movie_dec=1;
