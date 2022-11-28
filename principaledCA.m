fed7488dfd969b45bd73bb2571cdd5295259da50
kimerein <kimerein@users.noreply.github.com>
2022-11-28 13:57:52 -0500

    zscore and smooth options for principaledCA.m

--- principaledCA.m
+++ principaledCA.m
@@ -7,10 +7,16 @@ else
 end
 
 % Normalize each unit's PSTH, don't min-subtract here bcz assume 0 is 0
-data=normalizeData(data,2);
+% data=normalizeData(data,2);
+data=ZscoreData(data,2);
+data=smoothData(data,2);
 
 % Flatten
-flatData=flatten(data,'mean',3);
+flatData=flatten(data,'max',2);
+
+% Display flattened data matrix
+figure();
+imagesc(flatData); title('flatData');
 
 % Fill in nans and infs
 flatData=noNansOrInfs(flatData);
@@ -25,11 +31,53 @@ plotPCA(flatData,plotN);
 
 end
 
+function data=ZscoreData(data,whichdim)
+
+if whichdim==1
+    sz=[size(data,1) 1 1];
+elseif whichdim==2
+    sz=[1 size(data,2) 1];
+elseif whichdim==3
+    sz=[1 1 size(data,3)];
+else
+    error('principaledCA.m works only for 3D data matrices');
+end
+data=data-repmat(mean(data,whichdim,'omitnan'),sz);
+data=data./repmat(std(data,0,whichdim,'omitnan'),sz);
+
+end
+
+function data=smoothData(data,whichdim)
+
+if whichdim==1
+    s=[1 2 3];
+elseif whichdim==2
+    s=[2 1 3];
+elseif whichdim==3
+    s=[3 1 2];
+else
+    error('principaledCA.m works only for 3D data matrices');
+end
+origdataorder=[1 2 3];
+data=permute(data,s);
+origdataorder=origdataorder(s);
+for i=1:size(data,2)
+    for j=1:size(data,3)
+        data(:,i,j)=smoothdata(data(:,i,j),'gaussian',3);
+    end
+end
+[~,si]=sort(origdataorder);
+data=permute(data,si);
+
+end
+
 function flatData=flatten(data,method,dim)
 
 switch method
     case 'mean'
         flatData=mean(data,dim,'omitnan');
+    case 'max'
+        flatData=max(data,[],dim,'omitnan');
     case 'expand'
 end
 sz=size(flatData);
@@ -63,12 +111,17 @@ function plotPCA(A,plotN)
 
 [coeff,score,latent,tsquared,explained,mu]=pca(A);
 % plot
+if plotN>size(coeff,2)
+    plotTo=size(coeff,2);
+else
+    plotTo=plotN;
+end
 figure();
-for i=1:plotN
+title('PCs');
+for i=1:plotTo
     subplot(plotN,1,i);
     plot(coeff(:,i));
 end
-title('PCs');
 
 end
 
@@ -91,18 +144,26 @@ eigVec_nbyn=eigVec_nbyn(:,si);
 [sorted_eigVal_tbyt,si]=sort(diag(eigVal_tbyt),'descend');
 eigVec_tbyt=eigVec_tbyt(:,si);
 
-figure(); 
-for i=1:plotN
+figure(); title('eigs of A * transposeA');
+if plotN>size(eigVec_nbyn,2)
+    plotTo=size(eigVec_nbyn,2);
+else
+    plotTo=plotN;
+end
+for i=1:plotTo
     subplot(plotN,1,i);
     plot(eigVec_nbyn(:,i));
 end
-title('eigs of A * transposeA');
 
-figure(); 
-for i=1:plotN
+figure(); title('eigs of transposeA * A');
+if plotN>size(eigVec_tbyt,2)
+    plotTo=size(eigVec_tbyt,2);
+else
+    plotTo=plotN;
+end
+for i=1:plotTo
     subplot(plotN,1,i);
     plot(eigVec_tbyt(:,i));
 end
-title('eigs of transposeA * A');
 
 end
