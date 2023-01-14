5ba1f962dc03fd476981c1fb1fb98cc95e6d57df
kimerein <kimerein@users.noreply.github.com>
2023-01-14 14:17:45 -0500

    saveBehaviorAlignmentsPhotometry is working

--- process_units_for_reaching_behavior.m
+++ process_units_for_reaching_behavior.m
@@ -90,6 +90,7 @@ data_loc_array=table2cell(readtable(dataTable,'Format','%s%s%s%u%s%s%s%s%s%u%u%s
 % data_loc_array{i,12}='none'; % if opto-tagging, which tag
 % data_loc_array{i,13}=false; % flag if penetration was not through str
 % data_loc_array{i,14}='green_ch'; % which photo ch to use for photometry
+% data_loc_array{i,15}='Z:\MICROSCOPE\Kim\WHISPER recs\dLight1\20210311\photometry aligned to beh'; % where to save beh-aligned photometry data
 
 %% 2. Auto-populate SU QC and behavior alignments -- for 84 sessions, running all of this takes about 12 hrs
 % Discarding trials where unit dead or moved away
@@ -97,7 +98,10 @@ for i=1:size(data_loc_array,1)
     % load tbt (trial by trial) data
     [physiology_tbt,beh2_tbt,behavior_tbt,photometry_tbt]=loadReachingExptPhysData(data_loc_array(i,:));
     % save photometry alignments to behavior, if photometry was acquired
-    saveBehaviorAlignmentsPhotometry(photometry_tbt,behavior_tbt,data_loc_array{i,8},'',data_loc_array{i,14}); 
+    if ~isempty(photometry_tbt)
+        mkdir(data_loc_array{i,15});
+        saveBehaviorAlignmentsPhotometry(photometry_tbt,behavior_tbt,data_loc_array{i,15},'',data_loc_array{i,14}); 
+    end
     % get spikes
     dd=dir(data_loc_array{i,7});
     disp(['Processing ' data_loc_array{i,7}]);
