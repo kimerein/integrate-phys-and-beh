9e267a5b2e38b798585c29338f1bf7803afa71f0
kimerein <kimerein@users.noreply.github.com>
2023-01-20 11:12:58 -0500

    added dprime plot

--- plotSU_contextAndOutcome.m
+++ plotSU_contextAndOutcome.m
@@ -23,10 +23,19 @@ a=load([loc '\uncued_drop\' un '_uncuedDrop.mat']);
 uncuedFailure.dataout=a.dataout;
 uncuedFailure.alignComp=a.alignComp;
 
+resp_h1_green=[];
+resp_h1_red=[];
+resp_h2_green=[];
+resp_h2_red=[];
+resp_h3_cyan=[];
+resp_h3_black=[];
+resp_h4_cyan=[];
+resp_h4_black=[];
+
 figure(); 
 h1=subplot(2,2,1);
-dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; maxy1=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth); 
-dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; maxy2=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth);
+dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; [maxy1,r]=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth); resp_h1_green=[resp_h1_green; r];
+dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; [maxy2,r]=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth); resp_h1_red=[resp_h1_red; r];
 h1max=max(maxy1,maxy2); 
 if plotDerivativeInstead==false 
     ylim([0, h1max]);
@@ -34,8 +43,8 @@ end
 text(3,0,'cued');
 
 h2=subplot(2,2,2);
-dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; maxy1=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth); 
-dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; maxy2=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth);
+dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; [maxy1,r]=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth); resp_h2_green=[resp_h2_green; r];
+dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; [maxy2,r]=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth); resp_h2_red=[resp_h2_red; r];
 h2max=max(maxy1,maxy2); 
 if plotDerivativeInstead==false 
     ylim([0, h2max]);
@@ -43,8 +52,8 @@ end
 text(3,0,'uncued');
 
 h3=subplot(2,2,3);
-dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; maxy1=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth); 
-dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; maxy2=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth);
+dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; [maxy1,r]=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth); resp_h3_cyan=[resp_h3_cyan; r];
+dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; [maxy2,r]=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth); resp_h3_black=[resp_h3_black; r];
 h3max=max(maxy1,maxy2); 
 if plotDerivativeInstead==false 
     ylim([0, h3max]);
@@ -52,8 +61,8 @@ end
 text(3,0,'success');
 
 h4=subplot(2,2,4);
-dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; maxy1=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth); 
-dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; maxy2=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth);
+dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; [maxy1,r]=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth); resp_h4_cyan=[resp_h4_cyan; r];
+dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; [maxy2,r]=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth); resp_h4_black=[resp_h4_black; r];
 h4max=max(maxy1,maxy2); 
 if plotDerivativeInstead==false 
     ylim([0, h4max]);
@@ -77,32 +86,32 @@ if ~isempty(units)
         uncuedFailure.alignComp=a.alignComp;
 
         axes(h1);
-        dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; maxy1=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth);
-        dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; maxy2=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth);
+        dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; [maxy1,r]=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth); resp_h1_green=[resp_h1_green; r];
+        dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; [maxy2,r]=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth); resp_h1_red=[resp_h1_red; r];
         h1max=max(h1max,max(maxy1,maxy2)); 
         if plotDerivativeInstead==false 
             ylim([0, h1max]);
         end
 
         axes(h2);
-        dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; maxy1=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth);
-        dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; maxy2=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth);
+        dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; [maxy1,r]=plotit(dataout,alignComp,'g',plotDerivativeInstead,gaussmooth); resp_h2_green=[resp_h2_green; r];
+        dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; [maxy2,r]=plotit(dataout,alignComp,'r',plotDerivativeInstead,gaussmooth); resp_h2_red=[resp_h2_red; r];
         h2max=max(h2max,max(maxy1,maxy2)); 
         if plotDerivativeInstead==false 
             ylim([0, h2max]);
         end
 
         axes(h3);
-        dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; maxy1=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth);
-        dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; maxy2=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth);
+        dataout=cuedSuccess.dataout; alignComp=cuedSuccess.alignComp; [maxy1,r]=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth); resp_h3_cyan=[resp_h3_cyan; r];
+        dataout=uncuedSuccess.dataout; alignComp=uncuedSuccess.alignComp; [maxy2,r]=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth); resp_h3_black=[resp_h3_black; r];
         h3max=max(h3max,max(maxy1,maxy2)); 
         if plotDerivativeInstead==false 
             ylim([0, h3max]);
         end
 
         axes(h4);
-        dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; maxy1=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth);
-        dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; maxy2=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth);
+        dataout=cuedFailure.dataout; alignComp=cuedFailure.alignComp; [maxy1,r]=plotit(dataout,alignComp,'c',plotDerivativeInstead,gaussmooth); resp_h4_cyan=[resp_h4_cyan; r];
+        dataout=uncuedFailure.dataout; alignComp=uncuedFailure.alignComp; [maxy2,r]=plotit(dataout,alignComp,'k',plotDerivativeInstead,gaussmooth); resp_h4_black=[resp_h4_black; r];
         h4max=max(h4max,max(maxy1,maxy2)); 
         if plotDerivativeInstead==false 
             ylim([0, h4max]);
@@ -110,9 +119,47 @@ if ~isempty(units)
     end
 end
 
+dodprime=true;
+if dodprime==true
+    figure();
+    subplot(2,2,1);
+    plot(linspace(-3,10,size(resp_h1_green,2)),dprime(resp_h1_green,resp_h1_red)); xlim([-3 +10]);
+
+    subplot(2,2,2);
+    plot(linspace(-3,10,size(resp_h2_green,2)),dprime(resp_h2_green,resp_h2_red)); xlim([-3 +10]);
+
+    subplot(2,2,3);
+    plot(linspace(-3,10,size(resp_h3_cyan,2)),dprime(resp_h3_cyan,resp_h3_black)); xlim([-3 +10]);
+
+    subplot(2,2,4);
+    plot(linspace(-3,10,size(resp_h4_cyan,2)),dprime(resp_h4_cyan,resp_h4_black)); xlim([-3 +10]);
 end
 
-function maxy=plotit(dataout,alignComp,c,plotDerivativeInstead,gaussmooth)
+end
+
+function dprimes=dprime(resp1,resp2)
+
+accuracies=nan(1,size(resp1,2));
+dprimes=nan(1,size(resp1,2));
+for tp=1:length(accuracies)
+    % for each timepoint
+    trythresh=0:0.01:max(max(resp1(:,tp),[],1,'omitnan'),max(resp2(:,tp),[],1,'omitnan'));
+    a1=nan(size(trythresh));
+    a2=nan(size(trythresh));
+    for i=1:length(trythresh)
+        grp1=resp1(:,tp); grp2=resp2(:,tp);
+        grp1_guess=grp1<trythresh(i); grp2_guess=grp2>=trythresh(i);
+        a1(i)=(sum(grp1_guess==1)+sum(grp2_guess==1))/(length(grp1)+length(grp2));
+        grp1_guess=grp1>=trythresh(i); grp2_guess=grp2<trythresh(i);
+        a2(i)=(sum(grp1_guess==1)+sum(grp2_guess==1))/(length(grp1)+length(grp2));
+    end
+    accuracies(tp)=max([a1; a2],[],'all','omitnan');
+    dprimes(tp)=2*norminv(accuracies(tp));
+end
+
+end
+
+function [maxy,yout]=plotit(dataout,alignComp,c,plotDerivativeInstead,gaussmooth)
 
 [~,ma]=nanmax(nanmean(alignComp.y,1));
 alignmentTime=alignComp.x(ma);
@@ -129,5 +176,6 @@ hold on;
 line([alignComp.x(ma)-alignmentTime alignComp.x(ma)-alignmentTime],[0 1],'Color','b');
 xlim([-3 +10]);
 % xlim([-1 +4]);
+yout=temp(dataout.x-alignmentTime>-3 & dataout.x-alignmentTime<+10);
 
 end
