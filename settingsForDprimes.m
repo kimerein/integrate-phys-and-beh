1c18c0d422b6cbca917b1a36f2c14b8731d2802b
kimerein <kimerein@users.noreply.github.com>
2023-04-18 16:02:32 -0400

    This is the correct dprime settings to account for preemptive reaching

--- settingsForDprimes.m
+++ settingsForDprimes.m
@@ -8,14 +8,14 @@ tims=0:timestep:(size(alltbt.times,2)-1)*timestep;
 disp(['Cue turns on at ' num2str(tims(ma)) ' seconds']);
 cuetimeat=tims(ma);
 
-settings.preCueWindow_start1=cuetimeat-0.14-1.4; %0; % define start of time window from trial onset, in seconds -- for first window, assuming that trial onset is 0 sec
-settings.preCueWindow_end1=cuetimeat-0.14-0.7; %1; % define end of time window from trial onset, in seconds -- for first window
+settings.preCueWindow_start1=cuetimeat-0.45; %0; % define start of time window from trial onset, in seconds -- for first window, assuming that trial onset is 0 sec
+settings.preCueWindow_end1=cuetimeat; %1; % define end of time window from trial onset, in seconds -- for first window
 % preCueWindow_start2=3.81; % define start of time window from trial onset, in seconds -- for second window
 % preCueWindow_end2=5.31; % define end of time window from trial onset, in seconds -- for second window
-settings.preCueWindow_start2=cuetimeat+8.3; %3; %8.5;  
-settings.preCueWindow_end2=cuetimeat+9; %4; %9.5; 
-settings.reachAfterCueWindow_start=-0.2; %-0.25; % after cue window in seconds, for hits, i.e., real cued reaching
-settings.reachAfterCueWindow_end=0.5; %0.75; % after cue window in seconds, for hits, i.e., real cued reaching
+settings.preCueWindow_start2=cuetimeat+0.5; %8.5;  
+settings.preCueWindow_end2=cuetimeat+0.95; %9.5; 
+settings.reachAfterCueWindow_start=-0.05; %-0.25; % after cue window in seconds, for hits, i.e., real cued reaching
+settings.reachAfterCueWindow_end=0.4; %0.75; % after cue window in seconds, for hits, i.e., real cued reaching
 
 % PREVIOUSLY USED
 % settings.preCueWindow_start1=0; % define start of time window from trial onset, in seconds -- for first window, assuming that trial onset is 0 sec
