a72902e7b2ef57ed0a84af7efdc779cd5cc8a192
kimerein <kimerein@users.noreply.github.com>
2023-05-07 21:39:21 -0400

    which days to plot

--- plotCuedAndUncuedReachingOverDays.m
+++ plotCuedAndUncuedReachingOverDays.m
@@ -1,4 +1,4 @@
-function plotCuedAndUncuedReachingOverDays(all_rr_cued,all_rr_uncued,days)
+function plotCuedAndUncuedReachingOverDays(all_rr_cued,all_rr_uncued,days,whichdays)
 
 cmap=colormap(cool(40));
 
@@ -6,6 +6,9 @@ figure();
 for i=1:size(all_rr_cued,2)
     allrrcued_forthisday=all_rr_cued(:,i);
     allrruncued_forthisday=all_rr_uncued(:,i);
+    if ~ismember(days(i),whichdays)
+        continue
+    end
     me_cued=nanmean(allrrcued_forthisday);
     me_uncued=nanmean(allrruncued_forthisday);
     se_cued=nanstd(allrrcued_forthisday)./sqrt(nansum(~isnan(allrrcued_forthisday)));
@@ -20,7 +23,7 @@ for i=1:size(all_rr_cued,2)
     line([me_uncued-se_uncued me_uncued+se_uncued],[me_cued me_cued],'Color',currc); hold on;
     line([me_uncued me_uncued],[me_cued-se_cued me_cued+se_cued],'Color',currc);
 end
-plot(nanmean(all_rr_uncued,1),nanmean(all_rr_cued,1),'Color','k');
+plot(nanmean(all_rr_uncued(:,ismember(days,whichdays)),1),nanmean(all_rr_cued(:,ismember(days,whichdays)),1),'Color','k');
 
 % daspect([1 1 1]);
 xlabel('Uncued reaching (reaches per sec)');
