875bacc5a7ea7d443b079a1589fa2128e57b5a0c
kimerein <kimerein@users.noreply.github.com>
2023-05-10 17:11:15 -0400

    paw position working

--- getPawPosition.py
+++ getPawPosition.py
@@ -4,6 +4,7 @@ from scipy.io import savemat
 import numpy as np
 import matplotlib.pyplot as plt 
 import cv2
+import statsmodels.api as sm
 
 def getPawPosition(
 ):
@@ -46,9 +47,12 @@ def getPawPosition(
                 # Save to .mat file
                 savemat(os.path.join(directory, filename[:-4]+'side'+'.mat'),{'pawPos_x':pawPos_x,'pawPos_y':pawPos_y})
                 pp = getPawPosData(directory,filename,bodyparts_under,p_cutoff)
-                pawPos_x = pp[0]
-                pawPos_y = pp[1]
-                savemat(os.path.join(directory, filename[:-4]+'under'+'.mat'),{'pawPos_x':pawPos_x,'pawPos_y':pawPos_y})
+                pawPos_x_under = pp[0]
+                pawPos_y_under = pp[1]
+                savemat(os.path.join(directory, filename[:-4]+'under'+'.mat'),{'pawPos_x':pawPos_x_under,'pawPos_y':pawPos_y_under})
+                X, Y, Z, X_from_under = twoDtothreeD(pawPos_x,pawPos_y,pawPos_x_under,pawPos_y_under)
+                #X, Y, Z, X_from_under = filt3D(X,Y,Z,X_from_under)
+                savemat(os.path.join(directory, filename[:-4]+'3D'+'.mat'),{'X':X,'Y':Y,'Z':Z,'X_from_under':X_from_under})
         elif filename.endswith(".avi"):
             # If there is no corresponding .h5 file with the same name, then create a .mat file with nan
             if os.path.exists(os.path.join(directory, filename[:-4]+'DLC_resnet50_Testing2DJan4shuffle1_500000.h5')):
@@ -65,6 +69,44 @@ def getPawPosition(
             savemat(os.path.join(directory, filename[:-4]+'under'+'.mat'),{'pawPos_x':pawPos_x,'pawPos_y':pawPos_y})
         else:
             continue
+
+
+def filt3D(X,Y,Z,X_from_under):
+    # Filter X, Y and Z
+    X_filt = arimafilt(X)
+    Y_filt = arimafilt(Y)
+    Z_filt = arimafilt(Z)
+    X_from_under_filt = arimafilt(X_from_under)
+    return X_filt, Y_filt, Z_filt, X_from_under_filt
+
+
+def arimafilt(X):
+    model = sm.tsa.ARIMA(X, order=(3,1,10))
+    results = model.fit()
+    filtered_data = results.fittedvalues
+    return filtered_data
+
+
+def twoDtothreeD(side_pawPos_x,side_pawPos_y,under_pawPos_x,under_pawPos_y):
+    # This uses my definitions of X, Y and Z
+    Z = side_pawPos_x # minus is more dorsal, plus is more ventral
+    # X is average of side_pawPos_y and under_pawPos_y
+    X = side_pawPos_y # minus is more toward pellet, plus is more toward perch/body
+    # Rotate under view to line up perch axis with Y axis and perch-to-pellet axis with X axis
+    #cosine = np.cos(-np.pi/4)
+    #sine = np.sin(-np.pi/4)
+    #rotationMatrix = np.array([[cosine, -sine], [sine, cosine]]) # rotate counterclockwise by 45 degrees
+    # For each element of under_pawPos_x and under_pawPos_y, rotate by 45 degrees
+    #under_pawPos_x_rotated = np.empty_like(under_pawPos_x) * np.nan
+    #under_pawPos_y_rotated = np.empty_like(under_pawPos_y) * np.nan
+    #for i in range(len(under_pawPos_x)):
+    #    under_pawPos_x_rotated[i],under_pawPos_y_rotated[i] = np.dot(rotationMatrix, np.array([under_pawPos_x[i],under_pawPos_y[i]]))
+    #under_pawPos_x_rotated = - under_pawPos_x_rotated # minus is more toward pellet, plus is more toward perch/body
+    #X_from_under = under_pawPos_x_rotated
+    #Y = under_pawPos_y_rotated
+    Y = under_pawPos_y
+    X_from_under = under_pawPos_x
+    return X,Y,Z,X_from_under
 
 
 def fillinwithnearest(data):
@@ -137,31 +179,33 @@ def getPawPosData(directory,filename,bodyparts,p_cutoff):
     # pawPos_x = np.cumsum(pawPos_x_change,0)
     # pawPos_y = np.cumsum(pawPos_y_change,0)
     
-    # Plot paw position x and y with color changing as function of index
-    plt.figure()
-    # plt.scatter(pawPos_x_change,pawPos_y_change,c=np.arange(len(pawPos_x_change)))
-    plt.scatter(pawPos_x,pawPos_y,c=np.arange(len(pawPos_x)))
-    # Drop nan from pawPos_x and pawPos_y
-    # pawPos_x = pawPos_x[~np.isnan(pawPos_x)]
-    # pawPos_y = pawPos_y[~np.isnan(pawPos_y)]
+    # # Plot paw position x and y with color changing as function of index
+    # plt.figure()
+    # # plt.scatter(pawPos_x_change,pawPos_y_change,c=np.arange(len(pawPos_x_change)))
+    # plt.scatter(pawPos_x,pawPos_y,c=np.arange(len(pawPos_x)))
+    # # Drop nan from pawPos_x and pawPos_y
+    # # pawPos_x = pawPos_x[~np.isnan(pawPos_x)]
+    # # pawPos_y = pawPos_y[~np.isnan(pawPos_y)]
+
     pawPos_x_change = pawPos_x_change[~np.isnan(pawPos_x_change)]
     pawPos_y_change = pawPos_y_change[~np.isnan(pawPos_y_change)]
-    # Drop pawPos_x and pawPos_y if they are both less than 100
-    # Plot line connecting points, but skip points where either x or y is less than 100
-    # plt.plot(pawPos_x[pawPos_x>100],pawPos_y[pawPos_x>100])
-    plt.plot(pawPos_x,pawPos_y)
-    # plt.plot(pawPos_x_change,pawPos_y_change)
-    # Set x limits
-    plt.xlim(0, 350)
-    plt.ylim(0, 350)
+    
+    # # Drop pawPos_x and pawPos_y if they are both less than 100
+    # # Plot line connecting points, but skip points where either x or y is less than 100
+    # # plt.plot(pawPos_x[pawPos_x>100],pawPos_y[pawPos_x>100])
+    # plt.plot(pawPos_x,pawPos_y)
+    # # plt.plot(pawPos_x_change,pawPos_y_change)
+    # # Set x limits
+    # plt.xlim(0, 350)
+    # plt.ylim(0, 350)
 
-    plt.xlim(0, 1500)
-    plt.ylim(0, 1500)
+    # plt.xlim(0, 1500)
+    # plt.ylim(0, 1500)
 
-    # plt.xlim(-500, 500)
-    # plt.ylim(-500, 500)
-    plt.colorbar()
-    plt.show()
+    # # plt.xlim(-500, 500)
+    # # plt.ylim(-500, 500)
+    # plt.colorbar()
+    # plt.show()
     return pawPos_x, pawPos_y, pawPos_x_change, pawPos_y_change
 
 
