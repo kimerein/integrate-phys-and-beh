7bca49f07eebede3a9a12d4f27466ad3594cccec
kimerein <kimerein@users.noreply.github.com>
2023-05-24 10:34:34 -0400

    make removeUnitFromResponse work for trial by trial data

--- removeUnitFromResponse.m
+++ removeUnitFromResponse.m
@@ -1,15 +1,13 @@
 function [newResponse,whichToTakeFromUnits]=removeUnitFromResponse(Response,whichRmv)
 
-warning('Function currently only works for unit averages, not trial by trial');
-
 f=find(Response.excluded'==0 & ismember(1:length(Response.excluded),find(whichRmv==1)));
 currIncluded=find(Response.excluded'==0);
 whichToTakeFromUnits=find(ismember(currIncluded,f));
 
 newResponse=Response;
 fi=fieldnames(newResponse);
-% find which trials correspond to units that you want to take
-taketrials=ismember(Response.fromWhichUnit,whichToTakeFromUnits);
+% find which trials correspond to units that you want to remove
+taketrials=~ismember(Response.fromWhichUnit,whichToTakeFromUnits);
 for i=1:length(fi)
     temp=newResponse.(fi{i});
     if size(temp,1)==length(currIncluded)
