f4465c21104b79fdc758fe1cf2258986ecec6953
kimerein <kimerein@users.noreply.github.com>
2023-05-29 15:27:11 -0400

    seems to work

--- summarizeTensorRegressionIterations.m
+++ summarizeTensorRegressionIterations.m
@@ -4,7 +4,7 @@ dd=dir(dir_by_rank);
 dd=dd(3:end);
 loss=nan(1,length(dd));
 sumByFac=zeros(length(dd),whatRank);
-mixedLoading=nan(length(dd),whatRank);
+mixedLoading=nan(length(dd),1);
 for i=1:length(dd)
     if ~isempty(regexp(dd(i).name,'it'))
         a=load([dd(i).folder sep dd(i).name sep 'results.mat']);
@@ -45,24 +45,33 @@ for i=1:length(loss)
     end
     scatter(1:size(sumByFac,2),sumByFac(i,:),[],c,'LineWidth',2);
     hold on;
-    plot(1:size(sumByFac,2),sumByFac(i,:),'LineWidth',1,'Color',c);
+    plot(1:size(sumByFac,2),sumByFac(i,:),'LineWidth',2,'Color',c);
 end
+
+figure();
+scatter(whatRank*ones(size(mixedLoading)),mixedLoading,[],'k');
+hold on;
+line([whatRank-0.2 whatRank+0.2],[nanmean(mixedLoading) nanmean(mixedLoading)],'Color','k');
 
 end
 
 function overall_mixedLoadingPenalty=getMixedLoading(neuron_fac)
 
-pairwiseDiffs=nan(size(neuron_fac,1),size(neuron_fac,2)*size(neuron_fac,2));
+pairwiseDiffs=nan(size(neuron_fac,1),size(neuron_fac,2)*size(neuron_fac,2)-size(neuron_fac,2));
 for i=1:size(neuron_fac,1) % across neurons
     l=1;
     for j=1:size(neuron_fac,2)
         for k=1:size(neuron_fac,2) % across pairs of factors
+            % skip if these are the same
+            if j==k
+                continue
+            end
             pairwiseDiffs(i,l)=neuron_fac(i,j)-neuron_fac(i,k);
             l=l+1;
         end
     end
 end
-mixedLoadingPenalty=nanmin(abs(pairwiseDiffs),2);
+mixedLoadingPenalty=nanmin(abs(pairwiseDiffs),[],2);
 overall_mixedLoadingPenalty=nansum(mixedLoadingPenalty);
 
 end
